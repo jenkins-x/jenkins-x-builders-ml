cbd62b94d08bff6d730f622a4c9e19f7821b7fa9
Andrew Bayer <andrew.bayer@gmail.com>
2019-08-23 13:32:31 -0400

    chore: Switch to just one create pr call
    Signed-off-by: Andrew Bayer <andrew.bayer@gmail.com>

--- update-bot.sh
+++ update-bot.sh
@@ -4,5 +4,4 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-jx step create pr chart --name gcr.io/jenkinsxio/builder-machine-learning --version ${VERSION} --repo https://github.com/jenkins-x/jenkins-x-platform.git
-jx step create pr chart --name gcr.io/jenkinsxio/builder-machine-learning-gpu --version ${VERSION} --repo https://github.com/jenkins-x/jenkins-x-platform.git
+jx step create pr chart --name gcr.io/jenkinsxio/builder-machine-learning --name gcr.io/jenkinsxio/builder-machine-learning-gpu --version ${VERSION} --repo https://github.com/jenkins-x/jenkins-x-platform.git
